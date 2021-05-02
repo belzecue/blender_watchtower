5e3237ccdcd2e1c9eee4005a2531b844b630da9b
Francesco Siddi <francesco.siddi@gmail.com>
2021-05-02 19:26:04 +0200

    Remove unneeded assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <EditBreakdown msg="Welcome to Edit Breakdown Web"/>
+    <EditBreakdown msg="Edit Breakdown Web"/>
   </div>
 </template>
 
