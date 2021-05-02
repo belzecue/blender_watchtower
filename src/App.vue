ebad51b7713f85e7027ce5d9337d5d8d46544ed3
Francesco Siddi <francesco.siddi@gmail.com>
2021-05-02 18:35:48 +0200

    Add video-js and rename to EditBreakdown

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EditBreakdown msg="Welcome to Edit Breakdown Web"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EditBreakdown from './components/EditBreakdown.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EditBreakdown
   }
 }
 </script>
---
+++ src/components/EditBreakdown.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <video-player :options="videoOptions"/>
+  </div>
+</template>
+
+<script>
+
+import VideoPlayer from "./VideoPlayer.vue";
+
+export default {
+  name: 'EditBreakdown',
+  components: {
+    VideoPlayer
+  },
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src:
+              "/spring.mp4",
+              type: "video/mp4"
+          }
+        ]
+      }
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
