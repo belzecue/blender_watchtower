6a02029d63868760d2900a12bcf20c2798157e53
Francesco Siddi <francesco.siddi@gmail.com>
2021-05-08 13:26:06 +0200

    Fix indentation

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <video ref="videoPlayer" class="video-js vjs-fluid"></video>
-    </div>
+  <div>
+    <video ref="videoPlayer" class="video-js vjs-fluid"></video>
+  </div>
 </template>
 
 <script>
@@ -9,29 +9,29 @@ import videojs from 'video.js';
 import './../../node_modules/video.js/dist/video-js.css'
 
 export default {
-    name: "VideoPlayer",
-    props: {
-        options: {
-            type: Object,
-            default() {
-                return {};
-            }
-        }
-    },
-    data() {
-        return {
-            player: null
-        }
-    },
-    mounted() {
-        this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
-            console.log('onPlayerReady', this);
-        })
-    },
-    beforeDestroy() {
-        if (this.player) {
-            this.player.dispose()
-        }
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+          return {};
+      }
     }
+  },
+  data() {
+    return {
+      player: null
+    }
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    })
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
 }
 </script>
