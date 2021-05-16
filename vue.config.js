5e86f22a37d1e2004c9864d7e3f16645c94eeb87
Francesco Siddi <francesco.siddi@gmail.com>
2021-05-16 15:31:08 +0200

    Add webpack-glsl- loader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.module
+      .rule('glsl')
+      .test(/\.glsl$/)
+      .use('webpack-glsl-loader')
+        .loader('webpack-glsl-loader')
+        .end()
+  }
 }
