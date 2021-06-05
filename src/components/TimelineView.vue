0ed6e03e49838c4d548aa0fc49d349cc5b903d78
Ines Almeida <britalmeida@gmail.com>
2021-06-05 16:51:08 +0000

    Timeline: visual tweaks and refactor layout code to be parametric

--- src/components/TimelineView.vue
+++ src/components/TimelineView.vue
@@ -13,7 +13,7 @@
 
 <script>
 
-import {UIRenderer, Rect} from '../shading';
+import { UIRenderer } from '../shading';
 
 export default {
   name: "TimelineView",
@@ -34,28 +34,37 @@ export default {
       uiRenderer: null,
       ui2D: null,
       uiElements: {
-        margin: {x: 20, y: 30}, // Spacing around the contents of the timeline canvas. One side, in px.
+        margin: {x: 15, top: 22, bottom: 7}, // Spacing around the contents of the timeline canvas. One side, in px.
         playhead: {
-          padY: 8,
+          padY: 8, // From the absolute top. Ignores 'margin'.
           triangle: {width: 12.0, height: 8.0, flatHeight: 4.0},
           lineWidth: 2.0,
           color: [0.85, 0.8, 0.7, 1.0],
           shadow: { radius: 1.5, color: [0, 0, 0, 0.5] },
         },
         timeline: {
-          pad: {x: 20, y: 22},
-          height: 50,
-          color: [0.15, 0.15, 0.15, 1.0],
+          padX: 20,
+          frame0Color: [0.36, 0.36, 0.36, 1.0],
         },
         sequences: {
-          height: 12,
-          corner: 2,
+          height: 6,
+          channelHeight: 16,
+          corner: 0,
         },
         shots: {
+          height: 27,
+          channelHeight: 40,
           lineWidth: 1,
-          corner: 1,
+          corner: 0,
           color: [0.3, 0.3, 0.3, 1.0],
         },
+        channels: {
+          namePadX: 8,
+          height: 24,
+          contentHeight: 12,
+          colorOdd: [0.167, 0.167, 0.167, 1.0],
+          colorEven: [0.21, 0.21, 0.21, 1.0],
+        }
       },
       timelineRange: { x: 0 , w: 100},
     }
@@ -101,7 +110,11 @@ export default {
     resizeCanvas: function (shouldDraw = true) {
       const canvasContainer = document.getElementById('canvas-timeline-container');
       this.canvas.width = canvasContainer.offsetWidth;
-      this.canvas.height = 85 + 25 * this.taskTypes.length;
+      this.canvas.height = this.uiElements.margin.top
+          + this.uiElements.sequences.channelHeight
+          + this.uiElements.shots.channelHeight
+          + this.uiElements.channels.height * this.taskTypes.length
+          + this.uiElements.margin.bottom;
 
       this.canvasText.width = this.canvas.width;
       this.canvasText.height = this.canvas.height;
@@ -137,39 +150,58 @@ export default {
       this.ui2D.font = "12px sans-serif";
       this.ui2D.textAlign = "left";
       this.ui2D.textBaseline = "top";
-      this.ui2D.shadowOffsetX = 2;
-      this.ui2D.shadowOffsetY = 2;
+      this.ui2D.shadowOffsetX = 1;
+      this.ui2D.shadowOffsetY = 1;
       this.ui2D.shadowBlur = 2;
       this.ui2D.shadowColor = 'rgba(0, 0, 0, 0.5)';
 
-      // Channel names area
+      // Calculate size and position of elements.
       const margin = this.uiElements.margin;
       let channelNamesWidth = this.ui2D.measureText("Sequences").width;
       for (const task of this.taskTypes) {
         channelNamesWidth = Math.max(channelNamesWidth, this.ui2D.measureText(task.name).width);
       }
-
-      // Timeline area
-      const timeline = this.uiElements.timeline;
-      const timelineX = margin.x + channelNamesWidth + timeline.pad.x;
-      const timelineY = timeline.pad.y;
+      const numChannels = this.taskTypes.length;
+      const channelStep = this.uiElements.channels.height;
+      const timelinePadX = this.uiElements.timeline.padX;
+      const timelineX = margin.x + channelNamesWidth + timelinePadX;
       const timelineW = rect.width - timelineX - margin.x;
-      const timelineH = timeline.height;
+      const timelineTop = margin.top;
+      const timelineBottom = rect.height - this.uiElements.margin.bottom;
+      const seqHeight = this.uiElements.sequences.height;
+      const seqChannelHeight = this.uiElements.sequences.channelHeight;
+      const seqTop = margin.top + Math.round((seqChannelHeight - seqHeight) / 2);
+      const shotHeight = this.uiElements.shots.height;
+      const shotChannelHeight = this.uiElements.shots.channelHeight;
+      const shotChannelTop = margin.top + seqChannelHeight;
+      const shotTop = shotChannelTop + Math.round((shotChannelHeight - shotHeight) / 2);
+      const channelStartY = margin.top + seqChannelHeight + shotChannelHeight;
+      const channelContentPadY = Math.round((channelStep - this.uiElements.channels.contentHeight) / 2);
+      const channelBGWidth = channelNamesWidth + timelinePadX + timelineW;
+      const channelColor0 = this.uiElements.channels.colorEven;
+      const channelColor1 = this.uiElements.channels.colorOdd;
+
+      // Update cached timeline horizontal range.
       this.timelineRange.x = timelineX;
       this.timelineRange.w = timelineW;
-      ui.addRect(timelineX, timelineY, timelineW, timelineH, timeline.color);
 
-      // Channel strips
-      let channelY = 98;
-      for (let i=1; i < this.taskTypes.length; i++) {
-        ui.addLine([timelineX, channelY], [timelineX + timelineW, channelY], 1, timeline.color);
-        channelY += 25;
+      // Draw channel strips background.
+      ui.addRect(margin.x, margin.top, channelBGWidth, seqChannelHeight, channelColor0);
+      ui.addRect(margin.x, shotChannelTop, channelBGWidth, shotChannelHeight, channelColor1);
+      let channelY = channelStartY;
+      for (let i=0; i < numChannels; i++) {
+        const color = i % 2 === 1 ? channelColor1 : channelColor0;
+        ui.addRect(margin.x, channelY, channelBGWidth, channelStep, color);
+        channelY += channelStep;
       }
+      // Render timeline start as a line between the channel names and the timeline content.
+      // The timeline content might not start at frame 0, so the line is important.
+      const frame0LineColor = this.uiElements.timeline.frame0Color;
+      ui.addLine([timelineX, timelineTop], [timelineX, timelineBottom], 1, frame0LineColor);
 
       // Draw shots and their tasks' status
       const shotsStyle = this.uiElements.shots;
-      const shotTop = timelineY + 19;
-      const shotHeight = timelineH - 23;
+      const taskHeight = this.uiElements.channels.contentHeight;
       for (const shot of this.shots) {
         const startPos = timelineX + shot.startFrame * timelineW / this.totalFrames;
         const endFrame = shot.startFrame + 1 + shot.durationSeconds * this.fps;
@@ -178,28 +210,24 @@ export default {
         ui.addFrame(startPos, shotTop, shotWidth, shotHeight, shotsStyle.lineWidth, shotsStyle.color, shotsStyle.corner);
 
         // Draw task statuses
-        channelY = 77;
+        channelY = channelStartY + channelContentPadY;
         for (const task of this.taskTypes) {
-          let color = timeline.color; // Fallback, as shots only optionally have each task.
           for (const taskStatus of shot.tasks) {
             if (taskStatus.task_type_id === task.id) {
               for (const status of this.taskStatuses) {
                 if (taskStatus.task_status_id === status.id) {
-                  color = status.color;
-                  ui.addRect(startPos, channelY, shotWidth, 17, color);
+                  ui.addRect(startPos, channelY, shotWidth, taskHeight, status.color);
                   break;
                 }
               }
               break;
             }
           }
-          channelY += 25;
+          channelY += channelStep;
         }
       }
 
       // Draw sequences
-      const seqTop = timelineY + 5;
-      const seqHeight = this.uiElements.sequences.height;
       const seqCorner = this.uiElements.sequences.corner;
       for (const sequence of this.sequences) {
         // Find continuous ranges of shots that belong to this sequence.
@@ -276,12 +304,15 @@ export default {
         triangle.width + 1, playhead.triangle.flatHeight + 1, playhead.color, 1
       );
 
-      let textY = margin.y;
-      this.ui2D.fillText("Sequences", margin.x, textY); textY += 22;
-      this.ui2D.fillText("Shots", margin.x, textY); textY += 29;
+      const textX = margin.x + this.uiElements.channels.namePadX;
+      let textY = margin.top + Math.round(seqChannelHeight / 2) - 6; // 6 = font size / 2
+      this.ui2D.fillText("Sequences", textX, textY);
+      textY = shotChannelTop + Math.round(shotChannelHeight / 2) - 6;
+      this.ui2D.fillText("Shots", textX, textY);
+      textY = channelStartY + Math.round(channelStep / 2) - 6;
       for (const task of this.taskTypes) {
-        this.ui2D.fillText(task.name, margin.x, textY);
-        textY += 25;
+        this.ui2D.fillText(task.name, textX, textY);
+        textY += channelStep;
       }
 
       // Draw the frame.
