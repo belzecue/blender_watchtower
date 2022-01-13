a82dde880e9a37b0a6ac4885026e8cb965bc0752
Pablo Vazquez <pablo@blender.org>
2022-01-13 13:44:11 -0300

    UI: Add links to About and Dashboard to topbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="app-toolbar">
-    <span>Watchtower</span>
     <select v-if="projects" @change="switchToProject" v-model="currentProjectSelected" class="ml-4 mt-2">
       <option v-for="project in projects" :key="project.id" :value="project.id">
         {{ project.name }}
       </option>
     </select>
+    <div class="app-links">
+      <ul>
+        <li><a href="/#/" title="Dashboard">Dashboard</a></li>
+        <li><a href="/about" title="About Watchtower">About Watchtower</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -30,5 +35,22 @@ export default {
 </script>
 
 <style scoped>
+.app-links {
+  margin-left: auto;
+}
 
+.app-links > ul {
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.app-links ul > li {
+  margin-left: var(--spacer-2)
+}
+
+.app-links a {
+  color: var(--text-color-hint)
+}
 </style>
